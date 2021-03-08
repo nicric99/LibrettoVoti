9e78804620efe0a6ebad11dbc6e4ace4db8bd7c9
nicol <nicol@LAPTOP-NF84TNS2.station>
2021-03-08 10:19:36 +0000

    Soluzione punto 2

--- src/main/java/it/polito/tdp/librettovoti/model/Libretto.java
+++ src/main/java/it/polito/tdp/librettovoti/model/Libretto.java
@@ -13,6 +13,33 @@ public class Libretto {
 		this.voti = new ArrayList<>(); // qui sto inizializzando il mio oggetto lista, this lo metto perchè la dichiaro
 										// a lvl di istanza
 	}
+	/*public String stamtpaVotiUguali(int punteggio) {
+		// questo metood non serve a niente se us una interfacccia grafica
+	}
+	public String votiUguali(int punteggio) {
+		// calcola la stringa che contiene i voti decisi e sarà il chiamante se vuole stamparli metodo più generale
+		// che mi permette 
+	}*/
+	public List<Voto> ListaVotiUguali(int punteggio){
+		// questa soluzione è ancora più generica che rimanda la lista dei voti che posso manipolare a mio piacimento
+		// ma il difetto di questo metodo espone all'esterno la struttura dati interna  che ho usato
+		// se io decidessi di organizzare in modo diverso sarebbe difficile da riorganizzare
+		List<Voto> risultato= new ArrayList<>();
+		for(Voto v:this.voti) {
+			if(v.getVoto()==punteggio)
+				risultato.add(v);// questo è un riferimento che è lo stesso oggetto che ha un riferimento in una nuova lista
+		}
+		return risultato;
+	}
+	public Libretto votiUguali(int punteggio) {
+		// questa soluzione è la migliore poichhè poi ho tutte le operazioni che posso fare che posso filtrare i dati rispetto agli altri
+		Libretto risultato=new Libretto();
+		for(Voto v:this.voti) {
+			if(v.getVoto()==punteggio)
+				risultato.add(v);
+		}
+		return risultato;
+	}
 
 public void add(Voto v) {
 	this.voti.add(v);// delega alla lista l'aggiunta
--- src/main/java/it/polito/tdp/librettovoti/model/TestLibretto.java
+++ src/main/java/it/polito/tdp/librettovoti/model/TestLibretto.java
@@ -1,7 +1,7 @@
 package it.polito.tdp.librettovoti.model;
 
 import java.time.LocalDate;
-
+import java.util.*;
 public class TestLibretto {
 
 	public static void main(String[] args) {
@@ -10,10 +10,17 @@ public class TestLibretto {
 		System.out.println("Stampo i voti del libretto");
 		Voto voto1= new Voto("Analisi 1",30,LocalDate.of(2019, 2, 15));
 		
+		libretto.add(new Voto("Chimica",25,LocalDate.of(2019, 9, 15)));
 		libretto.add(voto1);
 		libretto.add(new Voto("Fisica 1", 28, LocalDate.of(2019, 7, 15)));
 		libretto.add(new Voto("Informatica",24,LocalDate.of(2019, 9, 15)));
+		
 		System.out.println(libretto); // mi serve però un metodo di stampa all'interno del libretto ma mi serve anche un metodo to string del voto
+		List<Voto> venticinque= libretto.ListaVotiUguali(25);
+		System.out.println(venticinque);
+		// usando il secondo metodi che restituisce un libretto abbiamo questo sintassi
+		Libretto librettoventicinque= libretto.votiUguali(25);
+		System.out.println(librettoventicinque);
 	}
 
 }
